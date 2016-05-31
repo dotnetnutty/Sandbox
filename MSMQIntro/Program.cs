83b70346f802c231feaa08210edb685ac6dc929b
Neil Pullinger <ncpullinger+github@gmail.com>
2016-05-31 10:03:07 +0100

    Adding MSMQ intro project

---
+++ MSMQIntro/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Messaging;
+
+namespace MSMQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var msgQue = MessageQueue.Create(@".\private$\aq");
+
+            string myString = "Somethingtoqueue";
+
+            var mq = new MessageQueue(@".\private$\aq", QueueAccessMode.Send);
+            mq.Send(myString, MessageQueueTransactionType.None);
+
+
+            var retrievedMessage = mq.Receive(new TimeSpan(1000));
+            string body = (string)retrievedMessage.Body;
+
+            Console.WriteLine("Retrieved body = " + body);
+        }
+    }
+}
